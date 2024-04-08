553f63a0f59dcb670f06d59d8f6daafc2572412a
Giulio Malventi <giulio.malventi@gmail.com>
2024-04-08 16:10:40 +0200

    Use transpile

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { bundle } from 'https://deno.land/x/emit@0.38.2/mod.ts'
+import { transpile } from 'https://deno.land/x/emit@0.38.3/mod.ts'
 import { Hono } from 'https://deno.land/x/hono@v4.2.2/mod.ts'
 
 const index = /* html */ `
@@ -15,14 +15,11 @@ const index = /* html */ `
   <script src="/client.js", type="module"></script>
 `
 
-const bundleClient = async () => (await bundle('./client.ts')).code
-const client = Deno.env.get('DENO_DEPLOYMENT_ID') ? null : await bundleClient()
-
 const app = new Hono()
   .get(
     '/client.js',
     async (c) =>
-      c.body(client || await bundleClient(), 200, {
+      c.body((await transpile(c.req.path)).get(c.req.path) ?? '', 200, {
         'content-type': 'application/javascript',
       }),
   )
