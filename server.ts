c467b61db84bb5a9739cbbc73593e02bcb61234c
Giulio Malventi <giulio.malventi@gmail.com>
2024-04-09 05:21:07 +0200

    Split index

--- server.ts
+++ server.ts
@@ -1,22 +1,10 @@
-import { Hono } from 'hono/mod.ts'
+import { Context, Hono } from 'hono/mod.ts'
 import { serveStatic } from 'hono/middleware.ts'
 
-const index = /* html */ `
-  <!DOCTYPE html>
-  ${
-  Deno.env.get('DENO_DEPLOYMENT_ID')
-    ? ''
-    : '<script src="http://localhost:35729/livereload.js?snipver=1"></script>'
-}
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-  <style>
-    input {width: 15em}
-  </style>
-  <script src="/client.js" type="module"></script>
-`
+import index from './views/index.ts'
 
 const app = new Hono()
   .use(serveStatic({ root: 'static' }))
-  .get((c) => c.html(index))
+  .get((c: Context) => c.html(index))
 
 Deno.serve(app.fetch)
---
+++ views/index.ts
@@ -0,0 +1,13 @@
+export default /* html */ `
+  <!DOCTYPE html>
+  ${
+  Deno.env.get('DENO_DEPLOYMENT_ID')
+    ? ''
+    : '<script src="http://localhost:35729/livereload.js?snipver=1"></script>'
+}
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+  <style>
+    input {width: 15em}
+  </style>
+  <script src="/client.js" type="module"></script>
+`
